4cc807b865849d61c7ecd3e1f1acc5e387bada15
NULL-000000 <black.design.mini@gmail.com>
2022-04-29 18:27:28 +0900

    style: 💄 revision styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { FormControl, List, TextField} from "@material-ui/core"
 import AddToPhotosIcons from "@material-ui/icons/AddToPhotos"
 import TaskItem from "./TaskItem";
+import { makeStyles } from "@material-ui/styles";
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import styles from "./App.module.css";
 import { db } from "./firebase";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: "auto",
+    width: "40%",
+  },
+});
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: ""}]);
   const [input, setInput] = useState("");
+  const classes = useStyles();
 
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
@@ -24,10 +37,12 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="App">
+    <div className={styles.app__root}>
       <h1>Todo App by React/firebase</h1>
+      <br />
       <FormControl>
         <TextField 
+          className={classes.field}
           InputLabelProps={{
             shrink: true,
           }}
@@ -38,11 +53,11 @@ const App: React.FC = () => {
           }
         />
       </FormControl>
-      <button disabled={!input} onClick={newTask}>
+      <button className={styles.app__icon} disabled={!input} onClick={newTask}>
         <AddToPhotosIcons />
       </button>
 
-      <List>
+      <List className={classes.list}>
         {tasks.map((task) => (
           <TaskItem key={task.id} id={task.id} title={task.title}></TaskItem>
         ))}
